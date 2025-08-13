4614327d118f31ba8a3d71f0ee4b171a03b1afa1
nyassky <gswarrios@gmail.com>
2025-08-13 20:43:21 +0500

    added the implementation of the success and delete buttons

--- app.js
+++ app.js
@@ -4,26 +4,62 @@ const priorityChoice = document.getElementById("priority");
 const typeOfTask = document.getElementById("type_of_task");
 const listElement = document.getElementById("list");
 const addTask = document.getElementById("add_task");
-const btnSuccess = document.getElementById("success");
-const btnDelete = document.getElementById("delete");
-console.log(taskInput.value)
-addTask.onclick = function () {
+const btnSuccess = document.querySelector(".success");
+const btnDelete = document.querySelector(".delete");
+const templateLi = document.querySelector(".template");
+
+function getTask (){
+	listElement.insertAdjacentHTML("beforeend", `
+        <li class="">
+			<div class="item_content">
+				<span>Task: ${taskInput.value}</span>
+		  		<span>Priority: ${priorityChoice.value}</span>
+				<span>Deadline: ${dateChoice.value}</span>
+				<span>Type: ${typeOfTask.value}</span>
+			</div>
+          	<div class="item_actions">
+            	<button class="success">&check;</button>
+            	<button class="delete">&times;</button>
+          	</div>
+        </li>
+	`)
+}
+function clearAll() {
+	taskInput.value = '';
+    dateChoice.value = '';
+    priorityChoice.value = '';
+    typeOfTask.value = '';
+}
+addTask.addEventListener("click", function (event){
+	event.preventDefault();
+
 	if (!taskInput.value || !dateChoice.value || !priorityChoice.value || !typeOfTask.value) {
     	alert("Пожалуйста, заполните все поля!");
     	return;
   	}
-	listElement.innerHTML = `
-        <li class="">
-			<div class="item_content">
-				<span>${taskInput.value}</span>
-		  		<span>${priorityChoice.value}</span>
-				<span>${dateChoice.value}</span>
-				<span>${typeOfTask.value}</span>
-			</div>
-          <div class="item_actions">
-            <button id="success">&check;</button>
-            <button id="delete">&times;</button>
-          </div>
-        </li>
-	`
-}
+
+	getTask();
+
+	clearAll();
+});
+
+listElement.addEventListener("click", (event) => {
+	event.preventDefault();
+	const target = event.target;
+
+	if (target.classList.contains("success")){
+		const listItem = target.closest("li");
+		listItem.style.background = "#89FC00";
+		const spans = listItem.querySelectorAll(".item_content span");
+		spans.forEach(span => {
+			span.style.textDecoration = "line-through";
+			span.style.color = "#484848";
+		});
+	}
+	if (target.classList.contains("delete")){
+		const listItem = target.closest("li");
+		listItem.remove();
+	}
+
+});
+
