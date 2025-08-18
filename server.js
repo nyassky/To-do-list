e4fb6b61ad6753646fea6fd44899bf63c9756ebd
nyassky <gswarrios@gmail.com>
2025-08-18 16:31:39 +0500

    added DB PostgreSQL and fixed bugs

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static("frontend"));
 const pool = new Pool ({
 	user: process.env.DB_USER,
 	password: process.env.DB_PASS,
